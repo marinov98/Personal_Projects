542e9bb126332a122655a87a7e8e2f8a0ee96f1a
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-05-12 19:55:19 -0400

    "improved main.cpp"

--- c++/statistical_calculator/main.cpp
+++ c++/statistical_calculator/main.cpp
@@ -5,43 +5,70 @@
 int main()
 {
   std::vector <double> dataset;
+  bool repeat;
   int terms;
   int trials=0;
   double inputnumber;
   std::string response;
+
   std::cout<<"~~~**Welcome to the statisical calculator MPM(beta stage)!**~~~"<<'\n';
   std::cout<<"What would you like the calculator to do?"<<'\n';
   std::cout<<"type 'basics'{mean,median,mode,range,standard deviation} or 'other' view more functions"<<'\n';
   std::cin>>response;
   if (response=="basics")
   {
-    Basics basics;
-    std::cout<<"How many terms are there?"<<'\n';
-    std::cin>>terms;
-    std::cout<<"You may now type the numbers in your data set"<<'\n';
-    while (trials!=terms)
+    repeat=true;
+    char answer;
+    while (repeat==true)
     {
-      std::cin>>inputnumber;
-      dataset.push_back(inputnumber);
-      trials++;
+      Basics basics;
+      //gets the number of terms in the date set and fills the vector with them
+      std::cout<<"How many terms are there?"<<'\n';
+      std::cin>>terms;
+      std::cout<<"You may now type the numbers in your data set"<<'\n';
+      while (trials!=terms)
+      {
+        std::cin>>inputnumber;
+        dataset.push_back(inputnumber);
+        trials++;
+      }
+      //showcases the dataset's mean, mode, and range
+      basics.displayDataset(dataset);
+      std::cout<<"The MEAN of the dataset is: "<<basics.calculateMean(dataset,terms)<<'\n';
+      std::cout<<"The MEDIAN of the data is: "<<basics.calculatePercentiles(dataset).q2<<'\n';
+      std::cout<<"The MODE of your dataset is: "<<basics.calculateMode(dataset)<<'\n';
+      std::cout<<"The RANGE of the dataset is: "<<basics.calculateRange(dataset)<<'\n';
+      basics.displayMinMax(dataset);
+      //prints the 25th,50th, and 75th percentile of the dataset
+      //basics.displayPercentiles(dataset);
+      if (dataset.size()>3)
+      {
+        std::cout<<'\n'<<"Percentiles: "<<'\n';
+        std::cout<<"The INTERQUARTILE RANGE of the dataset is: "<<basics.calculateInterquartileRange(dataset)<<'\n';
+        basics.displayPercentiles(dataset);
+      }
+      basics.findOutliers(dataset);
+
+      std::cout<<'\n';
+      //prints standard deviation and variance
+      std::cout<<"Standard deviation and Variance: "<<'\n';
+      basics.displayStandardDeviation(dataset,terms);
+      std::cout<<'\n';
+      //erase terms once finished showing numbers to user
+      dataset.erase(dataset.begin(),dataset.end());
+      //reset the trials
+      trials=0;
+      std::cout<<"Would like to use this function again? Type 'y' or 'n' "<<'\n';
+      std::cin>>answer;
+      if (answer=='y')
+      {
+        repeat=true;
+      }
+      else
+      {
+        repeat=false;
+      }
     }
-    basics.displayDataset(dataset);
-    std::cout<<"The MEAN of the dataset is: "<<basics.calculateMean(dataset,terms)<<'\n';
-    std::cout<<"The MODE of your dataset is: "<<basics.calculateMode(dataset)<<'\n';
-    std::cout<<"The RANGE of the dataset is: "<<basics.calculateRange(dataset)<<'\n';
-    basics.displayMinMax(dataset);
-    std::cout<<'\n'<<"Percentiles: "<<'\n';
-    basics.displayPercentiles(dataset);
-    std::cout<<"The INTERQUARTILE RANGE of the dataset is: "<<basics.calculateInterquartileRange(dataset)<<'\n';
-    basics.findOutliers(dataset);
-    std::cout<<'\n';
-    std::cout<<"Standard deviation and Variance: "<<'\n';
-    basics.displayStandardDeviation(dataset,terms);
-    std::cout<<'\n';
-    //erase terms once finished showing numbers to user
-    dataset.erase(dataset.begin(),dataset.end());
-    //reset the trials
-    trials=0;
   }
   else if (response=="other")
   {
