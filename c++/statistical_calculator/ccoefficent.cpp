38badfd2b79d7de3a7c57af688d1b3eae4793b3e
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-05-14 18:10:29 -0400

    "started confidence interval"

--- c++/statistical_calculator/ccoefficent.cpp
+++ c++/statistical_calculator/ccoefficent.cpp
@@ -1,1 +1,57 @@
-//corelation coeficient
+// corelation coeficient
+#include "basics.h"
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+double Interval::getZcritical(float percent)
+{
+	CriticalZ zcrit;
+	if (percent == .99)
+	{
+		return zcrit.z99;
+	}
+	else if (percent == .95)
+	{
+		return zcrit.z95;
+	}
+	else if (percent == .90)
+	{
+		return zcrit.z90;
+	}
+	else if (percent == .85)
+	{
+		return zcrit.z85;
+	}
+	else if (percent == .80)
+	{
+		return zcrit.z80;
+	}
+}
+
+double Interval::calculateSamplingDistributionSd(double sd, int sample_size)
+{
+	double samplingsd = (sd / std::sqrt(sample_size));
+	return samplingsd;
+}
+
+Bound Interval::calculateConfidenceInterval_Z(float percent,
+                                              double point_estimate,
+                                              double sd,
+                                              int sample_size)
+{
+	Bound bound;
+	const double zcrit = getZcritical(percent);
+	double new_sd = calculateSamplingDistributionSd(sd, sample_size);
+	bound.lower = (point_estimate - zcrit * new_sd);
+	bound.upper = (point_estimate + zcrit * new_sd);
+	return bound;
+}
+
+const void Interval::displayZConfidenceInterval(float percent, double point_estimate, double sd)
+{
+	const Bound bound =
+	    calculateConfidenceInterval_Z(float percent, double point_estimate, double sd);
+	std::cout << "Your Confidence interval is: "
+	          << "(" << bound.lower << ", " << bound.upper << ")" << '\n';
+}
--- c++/statistical_calculator/confidence.h
+++ c++/statistical_calculator/confidence.h
@@ -1,1 +1,35 @@
-//Confidence intervals
+// Confidence intervals
+// z-critical and t-critical
+#include "basics.h"
+#include <vector>
+
+struct Bound
+{
+	double lower;
+	double upper;
+};
+struct CriticalZ
+{
+	double z99;
+	double z95;
+	double z90;
+	double z85;
+	double z80;
+};
+
+class Interval
+{
+  private:
+	CriticalZ.z99 = 2.576;
+	CriticalZ.z95 = 1.960;
+	CriticalZ.z90 = 1.645;
+	CriticalZ.z85 = 1.440;
+	CriticalZ.z80 = 1.282;
+
+	Bound calculateConfidenceInterval_Z(float percent, double point_estimate, double sd);
+	double getZcritical(float percent);
+
+  public:
+	const void displayZConfidenceInterval(float percent, double point_estimate, double sd);
+	double calculateSamplingDistributionSd(double sd, int sample_size);
+};
