b1a656088deac04e02f435ce1103f1b0097cdeb0
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-05-13 20:02:18 -0400

    "fixed minor errors and applied formatting"

--- c++/statistical_calculator/basics.cpp
+++ c++/statistical_calculator/basics.cpp
@@ -5,244 +5,236 @@
 #include <vector>
 #include <cmath>
 #include "basics.h"
+#include <algorithms>
 
 Basics::Basics()
 {
-  sum=0;
-  mean=0;
-  mode=0;
-  range=0;
-  iqr=0;
+        sum=0;
+        mean=0;
+        mode=0;
+        range=0;
+        iqr=0;
 
 }
 //sorts the vector
 //TODO:search up better sorting algorithms(std::sort)
 void Basics::sortDataset(std::vector<double> &dataset)
 {
-  double hold;
-  for (int x=0;x<dataset.size();x++)
-  {
-    for (int y=0;y<dataset.size()-1;y++)
-    {
-      if (dataset[y] > dataset[y+1])
-      {
-        hold=dataset[y];
-        dataset[y]=dataset[y+1];
-        dataset[y+1]=hold;
-      }
-    }
-  }
+        double hold;
+        for (int x=0; x<dataset.size(); x++)
+        {
+                for (int y=0; y<dataset.size()-1; y++)
+                {
+                        if (dataset[y] > dataset[y+1])
+                        {
+                                hold=dataset[y];
+                                dataset[y]=dataset[y+1];
+                                dataset[y+1]=hold;
+                        }
+                }
+        }
 }
 
 //displays all the elements in the vector
-void Basics::displayDataset(std::vector<double> dataset) {
-	sortDataset(dataset);
-	// TODO:NOTE(sirflankalot): input -> inputted
-	std::cout << "The dataset you input is: " << '\n';
-	// TODO:NOTE(sirflankalot): for (auto& number : dataset)
-	// TODO:NOTE(sirflankalot): then dataset[index] -> number
-	for (int index = 0; index < dataset.size(); index++) {
-		std::cout << " " << dataset[index];
-	}
-	std::cout << '\n';
+const void Basics::displayDataset(std::vector<double> dataset)
+{
+        sortDataset(dataset);
+        std::cout << "The dataset you inputted is: " << '\n';
+        // TODO:NOTE(sirflankalot): for (auto& number : dataset)
+        // TODO:NOTE(sirflankalot): then dataset[index] -> number
+        for (int index = 0; index < dataset.size(); index++)
+        {
+                std::cout << " " << dataset[index];
+        }
+        std::cout << '\n';
 }
 
 
 void Basics::displayMinMax(std::vector<double> dataset)
 {
-  //TODO: NOTE(sirflankalot): dataset.first()
-	//TODO: NOTE(sirflankalot): dataset.last()
-  sortDataset(dataset);
-  std::cout<<"The minimum value of the dataset is: "<<dataset[0]<<'\n';
-  std::cout<<"The maximum value of the dataset is: "<<dataset[dataset.size()-1]<<'\n';
+        sortDataset(dataset);
+        std::cout<<"The minimum value of the dataset is: "<<dataset.first()<<'\n';
+        std::cout<<"The maximum value of the dataset is: "<<dataset.last()<<'\n';
 }
 
 void Basics::displayPercentiles(std::vector<double> dataset)
 {
-  // TODO:NOTE(sirflankalot):add a const
-  Percentile percentile=calculatePercentiles(dataset);
-  if (dataset.size()<=3)
-  {
-    std::cout<<"The 50th percentile(MEDIAN) of the dataset is: "<<percentile.q2<<'\n';
-  }
-  else if(dataset.size()>=4)
-  {
-    std::cout<<"The 25th percentile of the dataset is: "<<percentile.q1<<'\n';
-    std::cout<<"The 50th percentile(MEDIAN) of the dataset is: "<<percentile.q2<<'\n';
-    std::cout<<"The 75th percentile of the dataset is: "<<percentile.q3<<'\n';
-  }
+        const Percentile percentile=calculatePercentiles(dataset);
+        if (dataset.size()<=3)
+        {
+                std::cout<<"The 50th percentile(MEDIAN) of the dataset is: "<<percentile.q2<<'\n';
+        }
+        else if(dataset.size()>=4)
+        {
+                std::cout<<"The 25th percentile of the dataset is: "<<percentile.q1<<'\n';
+                std::cout<<"The 50th percentile(MEDIAN) of the dataset is: "<<percentile.q2<<'\n';
+                std::cout<<"The 75th percentile of the dataset is: "<<percentile.q3<<'\n';
+        }
 }
 
-// TODO:NOTE(sirflankalot): add a const to terms
 void Basics::displayStandardDeviation(std::vector <double> dataset,const int terms)
 {
-  //TODO: NOTE(sirflankalot): add a const
-	dataType sd = calculateStandardDeviation(dataset, terms);
-	// TODO:NOTE(sirflankalot): STOP YELLING AT ME
-	std::cout << "The SAMPLE STANDARD DEVIATION (Sx) is: " << sd.sample << '\n';
-	// TODO:NOTE(sirflankalot): Don't use pow, multiply by itself
-	std::cout << "The SAMPLE VARIANCE (Sx^2) is: " << pow(sd.sample, 2) << '\n';
-	std::cout << "The POPULATION STANDARD DEVIATION (σ) is: " << sd.population << '\n';
-	std::cout << "The POPULATION VARIANCE (σ^2) is: " << pow(sd.population, 2) << '\n';
+        const dataType sd = calculateStandardDeviation(dataset, terms);
+        // TODO:NOTE(sirflankalot): STOP YELLING AT ME
+        std::cout << "The SAMPLE STANDARD DEVIATION (Sx) is: " << sd.sample << '\n';
+        std::cout << "The SAMPLE VARIANCE (Sx^2) is: " <<(sd.sample*sd.sample)<< '\n';
+        std::cout << "The POPULATION STANDARD DEVIATION (σ) is: " <<sd.population << '\n';
+        std::cout << "The POPULATION VARIANCE (σ^2) is: " <<(sd.population*sd.population)<< '\n';
 }
 //finds the mean of the amount of terms inputed by the user
 double Basics::calculateMean(std::vector <double> dataset,const int terms)
 {
-  // TODO:NOTE(sirflankalot): std::accumulate in <numeric>
-  for (int i=0;i<dataset.size();i++)
-  {
-    sum+=dataset[i];
-  }
-  mean=(sum/terms);
-  return mean;
+        // TODO:NOTE(sirflankalot): std::accumulate in <numeric>
+        for (int i=0; i<dataset.size(); i++)
+        {
+                sum+=dataset[i];
+        }
+        mean=(sum/terms);
+        return mean;
 }
 
 double Basics::calculateRange(std::vector <double> dataset)
 {
-  // TODO:NOTE(sirflankalot): dataset.first()
-	// TODO:NOTE(sirflankalot): dataset.last()
-  sortDataset(dataset);
-  range=dataset[dataset.size()-1]-dataset[0];
-  return range;
+        sortDataset(dataset);
+        range=dataset.last()-dataset.first();
+        return range;
 }
 
 //calculates 25th,50th, and 75th percentile of the data set
 Percentile Basics::calculatePercentiles(std::vector <double> dataset)
 {
-  Percentile percentile;
-  sortDataset(dataset);
-  // TODO:NOTE(sirflankalot): this function is way to unweildy. Factor it out into three separate functions finding q1, q2, and q3.
+        Percentile percentile;
+        sortDataset(dataset);
+        // TODO:NOTE(sirflankalot): this function is way to unweildy. Factor it out into three separate functions finding q1, q2, and q3.
 
-	// TODO:NOTE(sirflankalot): factor this out into a boolean with a clear name like even_count
-  if (dataset.size() % 2 == 1 )
-  {
-    if ((dataset.size()/2-1) % 2 == 1)
-    {
-      percentile.q1=dataset[(dataset.size()-1)/4];
-      percentile.q2=dataset[(dataset.size()-1)/2];
-      percentile.q3=dataset[dataset.size()*3/4];
-    }
-    else
-    {
-      percentile.q1=((dataset[dataset.size()/4]+dataset[dataset.size()/4-1])/2);
-      percentile.q2=dataset[(dataset.size()-1)/2];
-      percentile.q3=((dataset[dataset.size()*3/4]+dataset[dataset.size()*3/4-1])/2);
-    }
-    return percentile;
-  }
-  // TODO:NOTE(sirflankalot): else is redundant
-  else
-  {
-    // TODO:NOTE(sirflankalot): Index out of range error with input 2 2 2
-    if ((dataset.size()/2) % 2 == 1)
-    {
-      percentile.q1=dataset[(dataset.size()-1)/4];
-      percentile.q2=((dataset[dataset.size()/2]+dataset[dataset.size()/2-1])/2);
-      percentile.q3=dataset[dataset.size()*3/4];
-    }
-    else
-    {
-      percentile.q1=((dataset[dataset.size()/4]+dataset[dataset.size()/4-1])/2);
-      percentile.q2=((dataset[dataset.size()/2]+dataset[dataset.size()/2-1])/2);
-      percentile.q3=((dataset[dataset.size()*3/4]+dataset[dataset.size()*3/4-1])/2);
-    }
-    return percentile;
-  }
+        // TODO:NOTE(sirflankalot): factor this out into a boolean with a clear name like even_count
+        if (dataset.size() % 2 == 1 )
+        {
+                if ((dataset.size()/2-1) % 2 == 1)
+                {
+                        percentile.q1=dataset[(dataset.size()-1)/4];
+                        percentile.q2=dataset[(dataset.size()-1)/2];
+                        percentile.q3=dataset[dataset.size()*3/4];
+                }
+                else
+                {
+                        percentile.q1=((dataset[dataset.size()/4]+dataset[dataset.size()/4-1])/2);
+                        percentile.q2=dataset[(dataset.size()-1)/2];
+                        percentile.q3=((dataset[dataset.size()*3/4]+dataset[dataset.size()*3/4-1])/2);
+                }
+                return percentile;
+        }
+        // TODO:NOTE(sirflankalot): else is redundant
+        else
+        {
+                // TODO:NOTE(sirflankalot): Index out of range error with input 2 2 2
+                if ((dataset.size()/2) % 2 == 1)
+                {
+                        percentile.q1=dataset[(dataset.size()-1)/4];
+                        percentile.q2=((dataset[dataset.size()/2]+dataset[dataset.size()/2-1])/2);
+                        percentile.q3=dataset[dataset.size()*3/4];
+                }
+                else
+                {
+                        percentile.q1=((dataset[dataset.size()/4]+dataset[dataset.size()/4-1])/2);
+                        percentile.q2=((dataset[dataset.size()/2]+dataset[dataset.size()/2-1])/2);
+                        percentile.q3=((dataset[dataset.size()*3/4]+dataset[dataset.size()*3/4-1])/2);
+                }
+                return percentile;
+        }
 }
 
 
 double Basics::calculateMode(std::vector <double> dataset)
 {
-  // TODO:NOTE(sirflankalot): dataset.first()
-  sortDataset(dataset);
-  mode = dataset[0];
-  int count=1;
-  int maxCount=0;
-  // TODO:NOTE(sirflankalot): Off by 1 error, you run off the end of the array here.
-  for (int i=1;i<dataset.size();i++)
-  {
-    if (dataset[i] == dataset[i+1])
-    {
-      count++;
-      if (count > maxCount)
-      {
-        maxCount=count;
-        mode=dataset[i];
-      }
-    }
-    else
-    {
-      count=1;
-    }
-  }
-  if (maxCount>0)
-  {
-    return mode;
-  }
-  else
-  {
-    // TODO:NOTE(sirflankalot): add space after comma
-    std::cout<<"all numbers repeat equally,returning first term: ";
-    return mode;
-  }
+        sortDataset(dataset);
+        mode = dataset.first();
+        int count=1;
+        int maxCount=0;
+        // TODO:NOTE(sirflankalot): Off by 1 error, you run off the end of the array here.(CHECK WITH CONOR FIRST!)
+        for (int i=1; i<dataset.size()-1; i++)
+        {
+                if (dataset[i] == dataset[i+1])
+                {
+                        count++;
+                        if (count > maxCount)
+                        {
+                                maxCount=count;
+                                mode=dataset[i];
+                        }
+                }
+                else
+                {
+                        count=1;
+                }
+        }
+        if (maxCount>0)
+        {
+                return mode;
+        }
+        else
+        {
+                std::cout<<"all numbers repeat equally, returning first term: ";
+                return mode;
+        }
 
 }
 
 DataType Basics::calculateStandardDeviation(std::vector <double> dataset,int terms)
 {
 
-  mean=calculateMean(dataset,terms);
-  DataType sd;
-  sd.population=0;
-  sd.sample=0;
-  // TODO:NOTE(sirflankalot): replace with foreach loop
-	// TODO:NOTE(sirflankalot): pow(blah, 2) -> blah * blah, use a temporary value
-  for (int i=0;i<dataset.size();i++)
-  {
-    sd.sample+=pow(dataset[i]-mean,2);
-    sd.population+=pow(dataset[i]-mean,2);
-  }
-  sd.sample=std::sqrt(sd.sample/(terms-1));
-  sd.population=std::sqrt(sd.population/(terms));
-  return sd;
+        mean=calculateMean(dataset,terms);
+        DataType sd;
+        sd.population=0;
+        sd.sample=0;
+        // TODO:NOTE(sirflankalot): replace with foreach loop
+        // TODO:NOTE(sirflankalot): pow(blah, 2) -> blah * blah, use a temporary value
+        for (int i=0; i<dataset.size(); i++)
+        {
+                double temp=dataset[i]-mean;
+                sd.sample+=(temp*temp);
+                sd.population+=(temp*temp);
+        }
+        sd.sample=std::sqrt(sd.sample/(terms-1));
+        sd.population=std::sqrt(sd.population/(terms));
+        return sd;
 }
 
 double Basics::calculateInterquartileRange(std::vector<double> dataset)
 {
-  percentile=calculatePercentiles(dataset);
-  iqr=percentile.q3-percentile.q1;
-  return iqr;
+        percentile=calculatePercentiles(dataset);
+        iqr=percentile.q3-percentile.q1;
+        return iqr;
 }
 
 
 void Basics::findOutliers(std::vector<double> dataset)
 {
-  // TODO:NOTE(sirflankalot): all 4 variables can be const
-  Percentile percentile=calculatePercentiles(dataset);
-  double iqr=calculateInterquartileRange(dataset);
-  double lower=percentile.q1-(iqr*1.5);
-  double upper=percentile.q3+(iqr*1.5);
+        const Percentile percentile=calculatePercentiles(dataset);
+        const double iqr=calculateInterquartileRange(dataset);
+        const double lower=percentile.q1-(iqr*1.5);
+        double upper=percentile.q3+(iqr*1.5);
 
-  // TODO:NOTE(sirflankalot): foreach loop
-  for (int i=0;i<dataset.size();i++)
-  {
-    if (dataset[i]>upper || dataset[i]<lower)
-    {
-      outliers.push_back(dataset[i]);
-    }
-  }
-  if (!(outliers.empty()))
-  {
-    std::cout<<"The OUTLIER(S) of this dataset is(are): ";
-    for (int j=0;j<outliers.size();j++)
-    {
-      std::cout<<outliers[j]<<" ";
-    }
-  }
-  else
-  {
-    std::cout<<"No outliers found in the data set";
-  }
+        // TODO:NOTE(sirflankalot): foreach loop
+        for (int i=0; i<dataset.size(); i++)
+        {
+                if (dataset[i]>upper || dataset[i]<lower)
+                {
+                        outliers.push_back(dataset[i]);
+                }
+        }
+        if (!(outliers.empty()))
+        {
+                std::cout<<"The OUTLIER(S) of this dataset is(are): ";
+                for (int j=0; j<outliers.size(); j++)
+                {
+                        std::cout<<outliers[j]<<" ";
+                }
+        }
+        else
+        {
+                std::cout<<"No outliers found in the data set";
+        }
 
-  std::cout<<'\n';
+        std::cout<<'\n';
 }
--- c++/statistical_calculator/basics.h
+++ c++/statistical_calculator/basics.h
@@ -5,47 +5,47 @@
 
 struct DataType
 {
-  double population;
-  double sample;
+        double population;
+        double sample;
 };
 
 struct Percentile
 {
-  double q1;
-  double q2;
-  double q3;
+        double q1;
+        double q2;
+        double q3;
 };
 
 
 class Basics
 {
-  private:
-    double mean;
-    double sum;
-    double mode;
-    double range;
-    double iqr;
-    Percentile percentile;
-    std::vector<double>outliers;
-  public:
-    //TODO:(sirflankalot): If the constructor is just going to initalize everything to zero, each member in the class should just have = 0 next to it, so:
-	  //TODO:(sirflankalot): mean = 0 etc.
-    Basics();
-    //TODO:(sirflankalot): See my write up for conceptual/organizational issues.
-	   // TODO:(sirflankalot): never pass by value unless you know that you will need a copy anyway.
-     //TODO:If you are just viewing the array, pass by const reference, if you're modifying, pass by non-const reference
-    //sorting/printing functions
-    void sortDataset(std::vector <double> &dataset);
-    void displayDataset(std::vector<double> dataset);
-    void displayStandardDeviation(std::vector <double> dataset,int terms);
-    void displayMinMax(std::vector<double> dataset);
-    void displayPercentiles(std::vector<double> dataset);
-    //calculation and functionality functions
-    double calculateMean(std::vector <double> dataset,int terms);
-    double calculateRange(std::vector <double> dataset);
-    Percentile calculatePercentiles(std::vector <double> dataset);
-    double calculateMode(std::vector <double> dataset);
-    dataType calculateStandardDeviation(std::vector <double> dataset,int terms);
-    double calculateInterquartileRange(std::vector<double> dataset);
-    void findOutliers(std::vector<double> dataset);
+private:
+double mean;
+double sum;
+double mode;
+double range;
+double iqr;
+Percentile percentile;
+std::vector<double>outliers;
+public:
+//TODO:(sirflankalot): If the constructor is just going to initalize everything to zero, each member in the class should just have = 0 next to it, so:
+//TODO:(sirflankalot): mean = 0 etc.
+Basics();
+//TODO:(sirflankalot): See my write up for conceptual/organizational issues.
+// TODO:(sirflankalot): never pass by value unless you know that you will need a copy anyway.
+//TODO:If you are just viewing the array, pass by const reference, if you're modifying, pass by non-const reference
+//sorting/printing functions
+void sortDataset(std::vector <double> &dataset);
+void displayDataset(const std::vector<double> dataset);
+void displayStandardDeviation(std::vector <double> dataset,int terms);
+void displayMinMax(std::vector<double> dataset);
+void displayPercentiles(std::vector<double> dataset);
+//calculation and functionality functions
+double calculateMean(std::vector <double> dataset,int terms);
+double calculateRange(std::vector <double> dataset);
+Percentile calculatePercentiles(std::vector <double> dataset);
+double calculateMode(std::vector <double> dataset);
+dataType calculateStandardDeviation(std::vector <double> dataset,int terms);
+double calculateInterquartileRange(std::vector<double> dataset);
+void findOutliers(std::vector<double> dataset);
 };
